f20efc0ac0386d6d7958a9159093634e36811c15
kambleadarsha98@gmail.com <kambleadarsha98@gmail.com>
2026-05-15 00:04:33 +0530

    new topic

--- Collections.py
+++ Collections.py
@@ -1,3 +1,5 @@
+#list 
+
 fruits  = [ "apple",  "banana",  "cherry", "mango"] 
 
 
@@ -28,3 +30,43 @@ fruits.pop()
 print(fruits)
 
 
+
+#tupples 
+
+
+
+point  = 10,  20 
+days =  ("mon","tue",  "wed", "thu" , "fri",  "sat", "sun") 
+
+print(point)
+
+print(point[0])
+print(days[2]) 
+
+dictionaries 
+
+
+isstudent = {
+    'name': 'kamble' ,
+    'age' : 22, 
+    'grade' : "A"
+ }
+
+
+print(isstudent) 
+
+print(isstudent['name']) 
+print(isstudent.get('age')) 
+
+
+isstudent['city'] = 'mumbai' 
+
+print(isstudent)
+
+del isstudent['city']
+
+print(isstudent)
+
+
+for key in  isstudent:
+    print(key)
